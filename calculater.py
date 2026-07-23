81c5770c21a6a582c7a75fdac5be96ecadb46e04
Rajjat Balbuddhe <rajjatbalbuddhe111@gmail.com>
2026-07-23 09:29:12 +0530

    now my calculater program is ready

--- calculater.py
+++ calculater.py
@@ -1,7 +1,23 @@
 num1 = int (input("Enter First Number: "))
 num2 = int (input("Enter Second Number: "))
 
+choice = input("Enter the operatin option (+, -, *, /): ")
+if choice == "+":
+    sum_of_num = num1 + num2
 
-sum_of_num = num1 + num2
+    print("Addition : ",sum_of_num)
 
-print(sum_of_num)
+elif choice == "-":
+    sub_of_number = num1 - num2
+    print("subtraction :" ,sub_of_number)
+
+elif choice == "*":
+    multiply_of_num = num1 * num2
+    print("multiplication : ",multiply_of_num)
+
+elif choice == "/":
+     print("divide :" ,divide_of_number)
+     divide_of_number = num1 / num2
+
+else:
+    ("inalid Choice")
