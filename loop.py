15b92347651f31040200040c6fce65f2a10a777a
Rajjat Balbuddhe <rajjatbalbuddhe111@gmail.com>
2026-07-23 11:08:08 +0530

    loop file add and edit

---
+++ loop.py
@@ -0,0 +1,19 @@
+list_of_name = ["Aman", "Prem", "aadi", "shubhan", "Ritik"]
+print(list_of_name)
+#added new name jon
+list_of_name.append("jon")
+#add one more new name Alen 
+list_of_name.append("Alen")
+print(list_of_name)
+
+list_of_name.insert(1, "Chetan")
+list_of_name.insert(3, "Tejas")
+list_of_name.insert(0, "Ritik")
+print(list_of_name)
+
+for name in list_of_name:
+    print(name)
+
+for i in range(1,11):
+    print("bro")
+    
